a4168723461a891addfb13a30c9bc5e2d83fe72f
fangjiayueyuan <fangjiayueyuan@163.com>
2020-03-04 17:52:48 +0800

    静态方法的使用

---
+++ demoStatic.py
@@ -0,0 +1,18 @@
+'''
+静态方法的学习
+'''
+class demoStatic:
+    count = 0
+    def __init__(self,name):
+        self.name = name
+        demoStatic.count+=1
+    @staticmethod
+    def foo():
+        demoStatic.count *= 100
+        return demoStatic.count
+
+dc1 = demoStatic("岳阳")
+dc2 = demoStatic("金榜")
+print(dc1.foo())
+print(demoStatic.foo())
+print(dc2.foo())
